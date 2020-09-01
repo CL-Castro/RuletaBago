9f19fc762bec6682c124537c0e994f55bba99108
Cristopher Castro <castro.inofuentes.cristopher@gmail.com>
2020-09-01 13:57:51 -0400

    Giro de ruleta y deteccion del resultado

---
+++ Ruleta/Assets/Game/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+
+public class GameController : MonoBehaviour
+{
+    [Header("Configuracion Ruleta")] public Image ruleta;
+    public float RotationSpeed;
+    private float _rotationSpeed;
+    public bool Giro;
+    public Text[] textos;
+    public GameObject puntero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rotationSpeed = RotationSpeed;
+        Giro = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Giro)
+        {
+            ruleta.transform.Rotate(Vector3.forward * ( RotationSpeed * Time.deltaTime ));
+            StartCoroutine(girando());
+            if (RotationSpeed <= 0)
+            {
+                Giro = false;
+                print("Se detuvo");
+                float dist = Int32.MaxValue;
+                string select = ""; 
+                foreach (Text txt in textos)
+                {
+                    if (Vector3.Distance(txt.transform.position, puntero.transform.position) < dist)
+                    {
+                        dist = Vector3.Distance(txt.transform.position, puntero.transform.position);
+                        select = txt.text;
+                    }
+                }
+                print("Seleccion -> "+select);
+                RotationSpeed = _rotationSpeed;
+            }
+        }
+        else
+        {
+            RotationSpeed = _rotationSpeed;
+        }
+    }
+
+    public void Girar()
+    {
+        if (!Giro)
+            Giro = true;
+    }
+
+    IEnumerator girando()
+    {
+        yield return new WaitForSeconds(1f);
+        RotationSpeed -= 0.5f;
+    }
+    
+    
+}
---
+++ Ruleta/Assets/Game/Scripts/PlayersController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SFB;
+using FlexFramework.Excel;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using sharpPDF;
+using sharpPDF.Enumerators;
+
+public class PlayersController : MonoBehaviour
+{
+    [Header("List Settings")] 
+    public Text NumOfPlayersInputField;
+    public int CurrentNumOfPlayers = 0;
+    // Start is called before the first frame update
+    public Text NumberOfPlayers;
+    public List<GameObject> Players;
+    public GameObject PlayerItemTemplate;
+    public GameObject Content;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreasePlayers()
+    {
+        NumberOfPlayers.text = (int.Parse(NumberOfPlayers.text) + 1).ToString();
+    }
+
+    public void DecreasePlayers()
+    {
+        NumberOfPlayers.text = (int.Parse(NumberOfPlayers.text) - 1).ToString();
+    }
+
+    public void AddPlayerItems()
+    {
+        for (int i = 0; i < int.Parse(NumberOfPlayers.text); i++)
+        {
+            var copy = Instantiate(PlayerItemTemplate);
+            copy.transform.SetParent(Content.transform);
+            copy.GetComponent<InputField>().text = "Jugador " + (i + 1);
+            Players.Add(copy);
+        }
+    }
+    
+    public void RestartData()
+    {
+        NumberOfPlayers.text = 0.ToString();
+        foreach (var player in Players)
+        {
+            Destroy(player);
+        }
+        Players.Clear();
+    }
+    
+    public void AddPlayersFromExcelFile()
+    {
+        string[] path = StandaloneFileBrowser.OpenFilePanel("Seleccione un archivo excel", "", "xlsx", false);
+        if (path.Length > 0)
+        {
+            RestartData();
+            var book = new WorkBook(path[0]);
+            var sheet = book[0];
+            for (int i = 0; i < sheet.Count; i++)
+            {
+                var row = sheet[i];
+                if (!row.IsEmpty())
+                {
+                    for (int j = 0; j < row.Count; j++)
+                    {
+                        if (row[j].Text.Length > 3)
+                        {
+                            Debug.Log(i + " player " + row[j].Text);
+                            AddPlayerItemFromExcel(i, row[j].Text);
+                        }
+                    }
+                }
+            }
+            CurrentNumOfPlayers = sheet.Count;
+            NumOfPlayersInputField.text = sheet.Count.ToString();
+        }
+    }
+    
+    public void AddPlayerItemFromExcel(int index, string name)
+    {
+        var copy = Instantiate(PlayerItemTemplate);
+        copy.transform.SetParent(Content.transform);
+        copy.GetComponent<Player>().index = index;
+        copy.GetComponent<InputField>().text = name;
+        Players.Add(copy);
+    }
+}
